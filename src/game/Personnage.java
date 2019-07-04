00824194b93e8600eb3e3bfe6dffb9ca1c428fde
alphanut <arnaud.seguin@gmail.com>
2019-07-05 00:27:31 +0200

    ajout des noms des attaques

--- src/game/Personnage.java
+++ src/game/Personnage.java
@@ -7,7 +7,26 @@ public abstract class Personnage {
     int _force;
     int _agilite;
     int _intelligence;
+    String _nomAttaqueBasique;
+    String _nomAttaqueSpeciale;
 
-    abstract void attaqueBasique(Personnage adversaire);
-    abstract void attaqueSpecial(Personnage adversaire);
+    /**
+     * Attaque de base qui cause des dommages à l'adversaire
+     * @param adversaire adversaire attaqué
+     */
+    public abstract void attaqueBasique(Personnage adversaire);
+
+    /**
+     * Attaque spéciale aux effets variés
+     * @param adversaire adversaire attaqué
+     */
+    public abstract void attaqueSpeciale(Personnage adversaire);
+
+    public String get_nomAttaqueBasique() {
+        return _nomAttaqueBasique;
+    }
+
+    public String get_nomAttaqueSpeciale() {
+        return _nomAttaqueSpeciale;
+    }
 }
